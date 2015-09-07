9a476ad831177f071ec9ebb47fe3b75d7069fc37
zuoyi <jianzuoyi@gmail.com>
2015-09-07 22:51:00 +0800

    add utils.py

---
+++ bin/utils.py
@@ -0,0 +1,16 @@
+'''
+EVAN	2015
+'''
+
+import sys
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+from Bio import SeqIO
+
+def main():
+	if len(sys.argv) < 3:
+		sys.stderr.write("Usage: fastatool.py <sub_command> <in.fasta>\n")
+		sys.exit()
+	
+if __name__ == '__main__':
+	main()
